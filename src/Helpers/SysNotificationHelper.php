bf0d938f55610692000a519c3667159a5f85dc92
drimcaster <drimcaster@gmail.com>
2024-06-12 14:41:50 +0800

    Update SysNotificationHelper.php

--- src/Helpers/SysNotificationHelper.php
+++ src/Helpers/SysNotificationHelper.php
@@ -22,13 +22,31 @@ class SysNotificationHelper
             return ["status"=>0,"message"=>"Failed to get logs."];
         }
 
-
+    
         //save log to notifications---
-        //$log_result - convert this to json and
+        $log_arr = [];
+
+
+        //$log_result - convert this to json and 
+        if(strlen($log_result) > 1){
+            $log_result = substr($log_result, 0, -1);
+            $log_arr = json_decode($log_result, TRUE);
+        }
+
+        if(!is_array($log_arr)){
+            ["status"=>0,"message"=>"Update logs error."];
+        }
+
+        if(count($log_arr)<= 0){
+            ["status"=>1,"message"=>"You are updated."];
+        }
+
+
+        //Saving updates to system notifications
 
 
 
 
-        return  ["status"=>1,"message"=>"Gather completed.", "data"=>$log_result];
+        return  ["status"=>1,"message"=>"Gather completed.", "data"=>$log_arr];
     }
 }
