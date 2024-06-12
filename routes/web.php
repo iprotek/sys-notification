4161d97b73242540818493c021678c3fdf38e5bf
drimcaster <drimcaster@gmail.com>
2024-06-12 11:54:07 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route; 
-use iProtek\SysNotification\Http\Controllers;
+use iProtek\SysNotification\Http\Controllers\SysNotificationController;
 
 //Route::prefix('sms-sender')->name('sms-sender')->group(function(){
   //  Route::get('/', [SmsController::class, 'index'])->name('.index');
 //});
 
 Route::prefix('manage/sys-notification')->name('manage.sys-notification')->middleware(['auth'])->group(function(){
-    Route::get('/test', [\iProtek\SysNotification\Http\Controllers\SysNotificationController::class, 'index'])->name('.test');
+    Route::get('/test', [SysNotificationController::class, 'index'])->name('.test');
 });
