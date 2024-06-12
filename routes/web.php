f67c8b0bf3a7f83b4669d71660611d52fcd59527
drimcaster <drimcaster@gmail.com>
2024-06-12 21:37:18 +0800

    route update

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ Route::middleware(['web'])->group(function(){
  
   Route::middleware(['auth'])->prefix('manage/sys-notification')->name('manage.sys-notification')->group(function(){
     Route::get('/test', [SysNotificationController::class, 'index'])->name('.test');
+    Route::get('/', [SysNotificationController::class, 'index'])->name('.index');
     Route::get('/system-updates', [SysNotificationController::class, 'system_updates'])->name('.system-updates');
     Route::post('/check-system-updates', [SysNotificationController::class, 'check_system_updates'])->name('.check-system-updates');
     Route::post('/appy-system-updates', [SysNotificationController::class, 'apply_system_updates'])->name('.apply-system-updates');
