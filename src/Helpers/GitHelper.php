bfc861c8c6ed5fd0e33bcbf6e031a9d981bacc8c
drimcaster <drimcaster@gmail.com>
2024-06-13 01:20:39 +0800

    update

--- src/Helpers/GitHelper.php
+++ src/Helpers/GitHelper.php
@@ -7,7 +7,7 @@ use Symfony\Component\Process\Process;
 class GitHelper
 {
     
-    public static function runGitCommand(string $command): string {
+    public static function runGitCommand(string $command){
         // Create a new Process instance with the given command
 
         $args = array_filter(explode(' ', $command));
