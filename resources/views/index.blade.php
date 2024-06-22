b3ca35833667668e7368953a7782ec7f9b31d7e6
drimcaster <drimcaster@gmail.com>
2024-06-22 20:50:40 +0800

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,38 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>iProtek - Sys Notification package</title>
-</head>
-<body>
-    <h1>Welcome to Your Package!</h1>
-</body>
-</html>
+
+@extends('iprotek_core::layout.pages.view-dashboard')
+@section('logout-link','/logout')
+@section('site-title', 'Notification')
+@section('head')
+    <link rel="stylesheet" href="/css/w3school/searchinput.css">
+    <link rel="stylesheet" href="/css/redtable.css">
+    <link rel="stylesheet" href="/css/Xpose-hover.css">
+    <script src="/js/xpose/Xpose-Events.js"></script>
+    <script src="/js/xlsx.full.min.js"></script>
+@endsection
+@section('breadcrumb')
+    <!--
+    <li class="breadcrumb-item"><a href="#">Home</a></li>
+    <li class="breadcrumb-item active">Widgets</li>
+    -->
+@endsection
+@section('content') 
+  <div id="main-content">
+  <?php
+    $user_id = auth()->user()->id;
+    $user_admin = \App\Models\UserAdminPayAccount::where('user_admin_id', $user_id)->first();
+    $group_id = 0;
+    if($user_admin){
+      $group_id = $user_admin->own_proxy_group_id;
+    }
+
+  ?> 
+  </div>
+  
+@endsection
+
+@section('foot')
+  <script>
+   // ActivateMenu(['menu-searches']);
+  </script>
+  <script src="/js/manage/projects-monitoring/searches.js"> </script>
+@endsection
