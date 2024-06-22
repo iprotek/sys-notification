d4f0ebed82ec2ca48b4f642d4592f2a8a2791902
drimcaster <drimcaster@gmail.com>
2024-06-23 02:28:17 +0800

    Update GitHelper.php

--- src/Helpers/GitHelper.php
+++ src/Helpers/GitHelper.php
@@ -16,9 +16,10 @@ class GitHelper
 
         if($is_composer){
             Log::error("TEST: ".base_path());
-            //array_unshift($args, base_path());
-            $process = new Process(["cd", base_path()]);
-            $process->run();
+            array_unshift($args, "cd");
+            array_unshift($args, base_path()."\r\n");
+            //$process = new Process(["cd", base_path()]);
+            //$process->run();
             $args[] = " --working-dir=".base_path();
             $base_path = base_path();
             $process = new Process($args, null, [
