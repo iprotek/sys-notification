db672183b7f8e3655b60d6c392d675e1656a6abd
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2025-05-31 16:43:59 +0800

    Update SysNotifyScheduleSmsTrigger.php

--- src/Models/SysNotifyScheduleSmsTrigger.php
+++ src/Models/SysNotifyScheduleSmsTrigger.php
@@ -9,4 +9,19 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class SysNotifyScheduleSmsTrigger extends _CommonModel
 {
     use HasFactory, SoftDeletes;
+
+    public $fillable = [
+        "sms_client_api_request_link_id",
+        "sms_notify_schedule_id",
+        "name",
+        "notification_type",
+        "to_type",
+        "selected_items",
+        "mobile_nos",
+        "total_due",
+        "total_paid",
+        "is_active",
+        "is_stop_when_fully_paid",
+        "error_message"
+    ];
 }
