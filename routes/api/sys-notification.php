f5e80da01cb09fb6cc3a7a7c33605fb0168f0eb6
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2026-05-01 00:34:34 +0800

    updates

--- routes/api/sys-notification.php
+++ routes/api/sys-notification.php
@@ -19,52 +19,119 @@ Route::prefix('/sys-notification')->name('.sys-notification')->group(function(){
     //Route::post('/get-content', [ CmsController::class ,'get_cms'])->name('.get'); 
     //Route::get('list', [XbranchController::class, 'branch_list'])->name('.list');
     
-    Route::get('to-type-list', function(Request $request){
-        return SysNotificationHelper::toTypeList();
-    })->name('.type-list');
+    Route::get('to-type-list', [ 
+        "uses"=>function(Request $request){
+            return SysNotificationHelper::toTypeList();
+        },
+        "description"=>"System Notification types",
+        "is_visible"=>false,
+        "is_allow"=>true
+    ])->name('.type-list');
     
 
 
     Route::prefix('schedulers')->name('.schedulers')->group(function(){
         
-        Route::get('list', [SysNotifySchedulerController::class, 'list'])->name('.list');
+        Route::get('list', [
+            "uses"=>[SysNotifySchedulerController::class, 'list'],
+            "description"=>"Notification scheduler lists",
+            "is_visible"=>false,
+            "is_allow"=>false
+        ])->name('.list');
 
-        Route::get('list/{scheduler_id}', [SysNotifySchedulerController::class, 'get'])->name('.get');
+        Route::get('list/{scheduler_id}', [
+            "uses"=>[SysNotifySchedulerController::class, 'get'],
+            "description"=>"Notification scheduler",
+            "is_visible"=>false,
+            "is_allow"=>false
+        ])->name('.get');
 
-        Route::post('add', [SysNotifySchedulerController::class, 'add'])->name('.add');
+        Route::post('add', [
+            "uses"=>[SysNotifySchedulerController::class, 'add'],
+            "description"=>"Add notification scheduler",
+            "is_visible"=>false,
+            "is_allow"=>false
+        ])->name('.add');
         
-        Route::put('update', [SysNotifySchedulerController::class, 'update'])->name('.update');
+        Route::put('update', [ 
+            "uses"=>[SysNotifySchedulerController::class, 'update'],
+            "description"=>"Update notification scheduler",
+            "is_visible"=>false,
+            "is_allow"=>false
+        ])->name('.update');
         
-        Route::delete('list/{scheduler_id}', [SysNotifySchedulerController::class, 'remove'])->name('.remove');
+        Route::delete('list/{scheduler_id}', [
+            "uses"=>[SysNotifySchedulerController::class, 'remove'],
+            "description"=>"Update branches from centralize controls",
+            "is_visible"=>false,
+            "is_allow"=>false
+        ])->name('.remove');
 
 
         Route::prefix('triggers')->name('.triggers')->group(function(){
 
             Route::prefix('sms')->name('.sms')->group(function(){
 
-                Route::get('list', [SysNotifyScheduleSmsTriggerController::class, 'list'])->name('.list');
+                Route::get('list', [ 
+                    "uses"=>[SysNotifyScheduleSmsTriggerController::class, 'list'],
+                    "description"=>"List of sms trigger to a schedule",
+                    "is_visible"=>false,
+                    "is_allow"=>false
+                ])->name('.list');
 
-                //Route::get('trigger-list/{schedule_sms_trigger_id}', [SysNotifyScheduleSmsTriggerController::class, 'trigger_list'])->name('.trigger-list');
-
-                Route::post('add', [SysNotifyScheduleSmsTriggerController::class, 'add'])->name('.add');
+                Route::post('add', [
+                    "uses"=>[SysNotifyScheduleSmsTriggerController::class, 'add'],
+                    "description"=>"Add sms trigger on a schedule",
+                    "is_visible"=>false,
+                    "is_allow"=>false
+                ])->name('.add');
                 
-                Route::put('update', [SysNotifyScheduleSmsTriggerController::class, 'update'])->name('.update');
+                Route::put('update', [
+                    "uses"=>[SysNotifyScheduleSmsTriggerController::class, 'update'],
+                    "description"=>"Update sms trigger on a schedule",
+                    "is_visible"=>false,
+                    "is_allow"=>false
+                ])->name('.update');
 
                 Route::prefix('get/{schedule_trigger_id}')->name('.get')->group(function(){
 
-                    Route::get('/', [SysNotifyScheduleSmsTriggerController::class, 'get'])->name('.get');
+                    Route::get('/', [
+                        "uses"=>[SysNotifyScheduleSmsTriggerController::class, 'get'],
+                        "description"=>"Get all trigger results on a specific trigger command in a schedule",
+                        "is_visible"=>false,
+                        "is_allow"=>false
+                    ])->name('.get');
                     
-                    Route::get('trigger-list', [SysNotifyScheduleSmsTriggerController::class, 'trigger_list'])->name('.trigger-list');
+                    Route::get('trigger-list', [
+                        "uses"=>[SysNotifyScheduleSmsTriggerController::class, 'trigger_list'],
+                        "description"=>"List of trigger in a schedule",
+                        "is_visible"=>false,
+                        "is_allow"=>false
+                    ])->name('.trigger-list');
 
                     Route::prefix('paid')->name('.paid')->group(function(){
                         
                         //LIST
-                        Route::get('list',[SysNotifyPaidScheduleTriggerController::class, 'paid_list'])->name('.list');
+                        Route::get('list',[ 
+                            "uses"=>[SysNotifyPaidScheduleTriggerController::class, 'paid_list'],
+                            "description"=>"Get paid list of specific trigger on a schedule",
+                            "is_visible"=>false,
+                            "is_allow"=>false
+                        ])->name('.list');
                         //ADD
-                        Route::post('add-pay',[SysNotifyPaidScheduleTriggerController::class, 'add_pay'])->name('.add-pay');
+                        Route::post('add-pay',[
+                            "uses"=>[SysNotifyPaidScheduleTriggerController::class, 'add_pay'],
+                            "description"=>"Add pay of specific trigger on a schedule",
+                            "is_visible"=>false,
+                            "is_allow"=>false
+                        ])->name('.add-pay');
                         //ADD NOTE
-                        Route::post('resend-sms-payment', [SysNotifyPaidScheduleTriggerController::class, 'resend_sms_payment'])->name('.resend-sms-payment');
-
+                        Route::post('resend-sms-payment', [
+                            "uses"=>[SysNotifyPaidScheduleTriggerController::class, 'resend_sms_payment'],
+                            "description"=>"Resend for payment info",
+                            "is_visible"=>false,
+                            "is_allow"=>false
+                        ])->name('.resend-sms-payment');
 
                     });
 
@@ -78,6 +145,11 @@ Route::prefix('/sys-notification')->name('.sys-notification')->group(function(){
     
     });
 
-    Route::get('clear', [SysNotificationController::class, 'clear_notification'])->name('.clear');
+    Route::get('clear', [
+        "uses"=>[SysNotificationController::class, 'clear_notification'],
+        "description"=>"Clear up notification every click",
+        "is_visible"=>false,
+        "is_allow"=>true
+    ])->name('.clear');
 
 });
--- routes/web.php
+++ routes/web.php
@@ -12,30 +12,90 @@ include(__DIR__.'/api.php');
 Route::middleware(['web'])->group(function(){
  
   Route::middleware(['auth:admin'])->prefix('manage/sys-notification')->name('manage.sys-notification')->group(function(){
-    Route::get('/test', [SysNotificationController::class, 'index'])->name('.test');
-    Route::get('/', [SysNotificationController::class, 'index'])->name('.index');
-    Route::get('/system-updates', [SysNotificationController::class, 'system_updates'])->name('.system-updates');
-    Route::post('/check-system-updates', [SysNotificationController::class, 'check_system_updates'])->name('.check-system-updates');
-    Route::get('/check-system-updates', [SysNotificationController::class, 'check_system_updates'])->name('.get-check-system-updates');
-    Route::get('/system-updates-summary', [SysNotificationController::class, 'system_updates_summary'])->name('.check-system-summary');
-    Route::post('/apply-system-updates', [SysNotificationController::class, 'apply_system_updates'])->name('.apply-system-updates');
+    //Route::get('/test', [SysNotificationController::class, 'index'])->name('.test');
+    Route::get('/', [
+      "uses"=>[SysNotificationController::class, 'index'],
+      "description"=>"Update branches from centralize controls",
+      "is_visible"=>false,
+      "is_allow"=>false
+    ])->name('.index');
+
+    Route::get('/system-updates', [
+      "uses"=>[SysNotificationController::class, 'system_updates'],
+      "description"=>"Render system updates",
+      "is_visible"=>false,
+      "is_allow"=>false
+    ])->name('.system-updates');
+
+    Route::post('/check-system-updates', [
+      "uses"=>[SysNotificationController::class, 'check_system_updates'],
+      "description"=>"Check system updates",
+      "is_visible"=>false,
+      "is_allow"=>false
+    ])->name('.check-system-updates');
+
+    Route::get('/check-system-updates', [
+      "uses"=>[SysNotificationController::class, 'check_system_updates'],
+      "description"=>"Check system updates",
+      "is_visible"=>false,
+      "is_allow"=>false
+    ])->name('.get-check-system-updates');
+
+    Route::get('/system-updates-summary', [
+      "uses"=>[SysNotificationController::class, 'system_updates_summary'],
+      "description"=>"Preview system updates",
+      "is_visible"=>false,
+      "is_allow"=>false
+    ])->name('.check-system-summary');
+
+    Route::post('/apply-system-updates', [
+      "uses"=>[SysNotificationController::class, 'apply_system_updates'],
+      "description"=>"Apply system updates",
+      "is_visible"=>false,
+      "is_allow"=>false
+    ])->name('.apply-system-updates');
   
-    Route::get('clear', [SysNotificationController::class, 'clear_notification'])->name('.clear');
+    Route::get('clear', [
+      "uses"=>[SysNotificationController::class, 'clear_notification'],
+      "description"=>"Clear notification",
+      "is_visible"=>false,
+      "is_allow"=>false
+    ])->name('.clear');
     
     Route::prefix('scheduler')->name('.manage-notify-scheduler')->group(function(){
 
-      Route::get('/', [SysNotifySchedulerController::class, 'index'])->name('.index');
+      Route::get('/', [
+        "uses"=>[SysNotifySchedulerController::class, 'index'],
+        "description"=>"Scheduler Index View",
+        "is_visible"=>false,
+        "is_allow"=>false
+      ])->name('.index');
     
       //TODO:: 
       Route::prefix('triggers')->name('.triggers')->group(function(){
 
-        Route::get('sms/{scheduler}', [SysNotifyScheduleSmsTriggerController::class, 'index'])->name('.sms-index');
+        Route::get('sms/{scheduler}', [
+          "uses"=>[SysNotifyScheduleSmsTriggerController::class, 'index'],
+          "description"=>"Sms triggers in a scheduler",
+          "is_visible"=>false,
+          "is_allow"=>false
+        ])->name('.sms-index');
 
         //TODO::Route::prefix('/email', )
-        Route::get('email/{scheduler}', [SysNotifyScheduleEmailTriggerController::class, 'index'])->name('.email-index');
+        Route::get('email/{scheduler}', [
+          "uses"=>[SysNotifyScheduleEmailTriggerController::class, 'index'],
+          "description"=>"Email triggers in a scheduler",
+          "is_visible"=>false,
+          "is_allow"=>false
+        ])->name('.email-index');
         
         //TODO::Route::prefix('/notification', )
-        Route::get('notification/{scheduler}', [SysNotifyScheduleNotificationTriggerController::class, 'index'])->name('.index');
+        Route::get('notification/{scheduler}', [
+          "uses"=>[SysNotifyScheduleNotificationTriggerController::class, 'index'],
+          "description"=>"System notification triggers in a scheduler",
+          "is_visible"=>false,
+          "is_allow"=>false
+        ])->name('.index');
 
       });
     
