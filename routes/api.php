3aa0d892fbe1f4cf140404ef42173b30477bd8ad
Joseph Aguilar <37214264+drimcaster@users.noreply.github.com>
2026-04-30 21:47:15 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('api')->middleware(['api'])->group(function(){
 
-    Route::prefix('group/{group_id}')->middleware(['pay.api'])->name('api')->group(function(){
+    Route::prefix('group/{group_id}')->middleware(['pay.api', 'policy.control'])->name('api')->group(function(){
         
         //FILE UPLOADS
         include(__DIR__.'/api/sys-notification.php');
